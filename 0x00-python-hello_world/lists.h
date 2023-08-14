187e24d7f70862ceab1bfd3eb7e54465f966b5b0
T-manDon <opiyoadiema@gmail.com>
2023-08-14 06:32:33 -0700

    0-run

--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -1,5 +1,4 @@
 #ifndef LISTS_H
-
 #define LISTS_H
 
 #include <stdlib.h>
@@ -10,11 +9,12 @@
  * @next: the next node pointer
  * Description: singy lists
  */
+
 typedef struct listint_s
-{
-int n;
-struct listint_s *next;
-} listint_t;
+	{
+	int n;
+	struct listint_s *next;
+	} listint_t;
 
 size_t print_listint(const listint_t *h);
 
