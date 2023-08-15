11839225b6cd5542ba9130a2fe9375f4c8fbf9e5
T-manDon <opiyoadiema@gmail.com>
2023-08-15 07:03:10 -0700

    lists

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=None):
-    """Print all integers of a list."""
+    """
+    Print all integers of a list.
+    """
     
     if my_list is None:
         my_list = []  # Create a new list if none is here
