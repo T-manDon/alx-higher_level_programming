0cb1a00d29a436ed95a057d61a021c434ad35c81
T-manDon <opiyoadiema@gmail.com>
2023-08-15 07:08:33 -0700

    link

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -4,9 +4,9 @@ def print_list_integer(my_list=None):
     """
     Print all integers of a list.
     """
-    
+
     if my_list is None:
         my_list = []  # Create a new list if none is here
-    
+
     for i in range(len(my_list)):
         print("{:d}".format(my_list[i]))
