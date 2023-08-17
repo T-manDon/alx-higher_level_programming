61cf95bc9adf2ee9c3405ecc444560b408b46e2f
T-manDon <opiyoadiema@gmail.com>
2023-08-17 09:59:05 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,15 +2,11 @@
 # 0-add.py
 
 if __name__ == "__main__":
-
     """
     Determines sumation of 1 and 2.
     """
-
     from add_0 import add
 
     a = 1
-
     b = 2
-
     print("{} + {} = {}".format(a, b, add(a, b)))
