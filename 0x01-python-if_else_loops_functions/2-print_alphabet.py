c41c613cbc1deb6d00e27462ac16705984247b1f
T-manDon <opiyoadiema@gmail.com>
2023-08-28 08:06:58 -0700

    2printy

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+# 2-print_alphabet.py
 
-"""
-Print the alphabet in lowercase, not followed by a new line.
-"""
+# This script prints the lowercase alphabet characters without newline characters.
 
+# Iterate through the ASCII values of lowercase letters (97 to 122)
 for letter in range(97, 123):
+    # Convert the ASCII value to the corresponding character and print it
     print("{}".format(chr(letter)), end="")
