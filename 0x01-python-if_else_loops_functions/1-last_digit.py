fc3ad87d3d9581c4b1b269e2bfe02fc9277e6251
T-manDon <opiyoadiema@gmail.com>
2023-08-28 08:01:38 -0700

    last dig

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python3
-# 1-last_digit.py
-
 import random
 
+# Generate a random integer between -10000 and 10000 (inclusive)
 number = random.randint(-10000, 10000)
 
+# Extract the last digit of the absolute value of the number
 digit = abs(number) % 10
 
+# If the original number was negative, adjust the sign of the digit
 if number < 0:
-
     digit = -digit
 
-print("The last digit of {} is {} and is ".format(number, digit), end="")
+# Print the beginning of the output statement
+print("Last digit of {} is {} and is ".format(number, digit), end="")
 
+# Check the value of the extracted digit and provide information accordingly
 if digit > 5:
-
     print("greater than 5")
-
 elif digit == 0:
-
     print("0")
-
 else:
-
     print("less than 6 and not 0")
