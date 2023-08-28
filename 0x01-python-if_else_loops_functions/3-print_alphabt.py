a5843b3e25f78c2f9deed04621ef6a44207608a7
T-manDon <opiyoadiema@gmail.com>
2023-08-28 15:09:53 -0700

    print

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-
-# Iterate through the ASCII values of lowercase letters (97 to 122)
 for letter in range(97, 123):
-    # Check if the character is not 'q' and not 'e'
     if chr(letter) is not 'q' and chr(letter) is not 'e':
-        # Print the character without newline
         print("{}".format(chr(letter)), end="")
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
 # 6-print_comb3.py
 
-# This script prints all possible different combinations of two digits in ascending order.
-# The two digits must be different - 01 and 10 are considered identical.
-
-# Iterate through the first digit from 0 to 9
 for digit1 in range(0, 10):
-    # Iterate through the second digit from digit1 + 1 to 9
     for digit2 in range(digit1 + 1, 10):
-        # Check if the current combination is 89
         if digit1 == 8 and digit2 == 9:
-            # If it's 89, print it without a comma and space
             print("{}{}".format(digit1, digit2))
         else:
-            # For other combinations, print them with a comma and space
             print("{}{}".format(digit1, digit2), end=", ")
