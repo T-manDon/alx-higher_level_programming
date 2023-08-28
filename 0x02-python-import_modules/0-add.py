8a049c3e036f21c2fafa0fd5379db7efec4773c3
T-manDon <opiyoadiema@gmail.com>
2023-08-28 14:33:55 -0700

    add

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
-# Check if this script is being run as the main program
 if __name__ == "__main__":
-    """Print the sum of 1 and 2."""
-
-    # Import the 'add' function from the 'add_0' module
     from add_0 import add
 
-    # Define the values to be added
     a = 1
     b = 2
-
-    # Print the sum using the 'add' function
     print("{} + {} = {}".format(a, b, add(a, b)))
