d6cf08d2a6a202a88c99a21246236b63fa4cd087
T-manDon <opiyoadiema@gmail.com>
2023-08-29 12:22:25 -0700

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+class Square:
+    """
+    This is the Square class.
+    """
+    def __init__(self, size):
+        """
+        Initializes a Square instance.
+
+        Args:
+            size (int): The size of the square.
+
+        Attributes:
+            __size (int): Private instance attribute for the size of the square.
+        """
+        self.__size = size
