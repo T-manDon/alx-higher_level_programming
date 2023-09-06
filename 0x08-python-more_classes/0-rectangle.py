687e571ebcda483cca0e24915f2c0acbb601aab5
T-manDon <opiyoadiema@gmail.com>
2023-09-06 14:21:51 -0700

    rectangular

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,3 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python3  # This is a shebang line specifying the Python interpreter to use.
+
+"""Defines a Rectangle class."""  # This is a module-level docstring providing an overview of the module.
+
 class Rectangle:
-    pass
+    """Represent a rectangle."""  # This is a class-level docstring describing the purpose of the `Rectangle` class.
+    pass  # This is a placeholder statement; you can add attributes and methods here later.
