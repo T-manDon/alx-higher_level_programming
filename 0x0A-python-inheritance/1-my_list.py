43226391c00a232391441fd79a7e0a6188d077d7
T-manDon <opiyoadiema@gmail.com>
2023-09-12 06:33:34 -0700

    same

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,18 +1,13 @@
 #!/usr/bin/python3
-
+"""
+MyList
+"""
 class MyList(list):
     """
-    Custom list class that inherits from list.
+    Contains lsting
     """
-
     def print_sorted(self):
         """
-        Prints the list in ascending order.
+        Print in sorted format
         """
-        sorted_list = sorted(self)
-        print(sorted_list)
-
-# Example usage:
-if __name__ == "__main__":
-    my_list = MyList([3, 1, 2, 5, 4])
-    my_list.print_sorted()
+        print(sorted(self))
--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,25 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Thes_same_class
+"""
 def is_same_class(obj, a_class):
     """
-    Checks if the object is exactly an instance of the specified class.
-
-    Args:
-    obj: Any Python object.
-    a_class: A Python class.
-
-    Returns:
-    True if obj is an instance of a_class, otherwise False.
+    Returns True or false
     """
-    return type(obj) is a_class
-
-# Example usage:
-if __name__ == "__main__":
-    class ExampleClass:
-        pass
-
-    obj1 = ExampleClass()
-    obj2 = "Hello, World!"
-    
-    print(is_same_class(obj1, ExampleClass))  # True
-    print(is_same_class(obj2, ExampleClass))  # False
+    if type(obj) is a_class:
+        return True
+    else:
+        return False
