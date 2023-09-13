e7de387c6e593d5c3b6ef06dd1ad69d1c5d4fa29
T-manDon <opiyoadiema@gmail.com>
2023-09-12 17:57:04 -0700

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 
 def read_file(filename=""):
-    try:
-        with open(filename, mode="r", encoding="utf-8") as file:
-            contents = file.read()
-            print(contents, end="")  # Print the file contents to stdout
-    except FileNotFoundError:
-        pass  # Do nothing if the file doesn't exist
+    with open(filename, encoding='utf-8') as file:
+        print(file.read(), end='')
