a8889d0b44d11fa2e28aebf024ed4c00cbde0861
T-manDon <opiyoadiema@gmail.com>
2023-09-12 18:11:20 -0700

    write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
+"""write_file.py """
 
 def write_file(filename="", text=""):
-    try:
-        with open(filename, mode="w", encoding="utf-8") as file:
-            num_characters_written = file.write(text)
-        return num_characters_written
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return 0
+    """
+    Write a string to a text file and return
+
+    Args:
+        filename (str): The file nam.
+        text (str): The string of character.
+
+    Returns:
+        int: The number of characters written.
+    """
+    # Open the file with 'w' flag to ensure existing content is replaced.
+    with open(filename, 'w', encoding='utf-8') as file:
+        chars_written = file.write(text)
+        return chars_written
