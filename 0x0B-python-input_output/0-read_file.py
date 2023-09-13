6df60211d4dac64bef7a2a2c0ca05d56b5dbc4e2
T-manDon <opiyoadiema@gmail.com>
2023-09-12 18:02:51 -0700

    read

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Input-Output
+"""
 def read_file(filename=""):
+    """
+    Reads text contents
+    Args:
+        filename (str): name of fil
+    """
     with open(filename, encoding='utf-8') as file:
         print(file.read(), end='')
